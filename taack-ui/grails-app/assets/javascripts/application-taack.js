abc83633ced643501f11795fc163798cee37d9bf
Adrien GUICHARD <adrien.guichard@citel.fr>
2023-12-23 20:23:42 +0100

    remove embedded chart.min.js. See taack/ui/dump/RawHtmlChartDump.groovy

--- taack-ui/grails-app/assets/javascripts/application-taack.js
+++ taack-ui/grails-app/assets/javascripts/application-taack.js
@@ -1,9 +1,6 @@
 //= require reveal.js/dist/reveal.js
 //= require reveal.js/plugin/highlight/highlight.js
 //= require reveal.js/plugin/zoom/zoom.js
-//// require jquery-3.3.1.min
-//// require popper.min
-//// require Chart.min
 //= require client
 //= require wysiwyg
 //= require_self
