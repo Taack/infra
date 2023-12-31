f7bdeabedd744b30f6a7981c2b0c065d301c4feb
Adrien GUICHARD <adrien.guichard@citel.fr>
2023-12-31 15:37:32 +0100

    WiP States for few countries in order to prepare address formatting helper methods.

--- taack-ui/src/main/groovy/taack/ui/config/AddressFormat.groovy
+++ taack-ui/src/main/groovy/taack/ui/config/AddressFormat.groovy
@@ -3,182 +3,6 @@ package taack.ui.config
 import groovy.transform.CompileStatic
 
 @CompileStatic
-enum DeliveryType {
-    CARE_PO('Care of post office', 'CARE PO'),
-    CMB('Community mail bag', 'CMB'),
-    GPO_BOX('General Post Box (in capital cities)', 'GPO BOX'),
-    MS('Mail service', 'MS'),
-    RSD('Roadside delivery', 'RSD'),
-    RMS('Roadside mail service', 'RMS'),
-    CMA('Community mail agent', 'CMA'),
-    CPA('Community postal agent\t', 'CPA'),
-    LOCKED_BAG('Locked bag', 'LOCKED BAG'),
-    RMB('Roadside mail box/bag', 'RMB'),
-    PRIVATE_BAG('Private bag', 'PRIVATE BAG')
-
-    DeliveryType(String label, String abbrev) {
-        this.label = label
-        this.abbrev = abbrev
-    }
-
-    String label
-    String abbrev
-}
-
-
-@CompileStatic
-enum AddressAreaType {
-    RURAL, URBAN, BUSINESS, RESIDENTIAL, PO_BOX, POST_RESTANT
-}
-
-// TODO: Bad idea, kept for Wikipedia links
-// https://en.wikipedia.org/wiki/House_numbering
-@CompileStatic
-enum AddressFormatSettings {
-    // https://en.wikipedia.org/wiki/Postal_codes_in_Argentina
-    ARGENTINA(true, false, true, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postcodes_in_Australia
-    // http://auspost.com.au/personal/addressing-guidelines.html
-    AUSTRALIA(false, false, false, true, true, true, false),
-    // https://en.wikipedia.org/wiki/Postal_codes_in_Austria
-    AUSTRIA(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postal_codes_in_Bangladesh
-    BANGLADESH(true, false, false, false, false, false, false),
-    BELARUS(true, false, false, false, false, false, false),
-    // https://web.archive.org/web/20130420085220/http://www.bpost.be/site/fr/residential/letters-cards/send/best_practices.html
-    // https://en.wikipedia.org/wiki/Bpost
-    // https://www.upu.int/en/Postal-Solutions/Programmes-Services/Addressing-Solutions#scroll-nav__5
-    BELGIUM(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Federative_units_of_Brazil
-    BRAZIL(true, false, false, false, false, false, false),
-    BULGARIA(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postal_codes_in_Canada
-    // https://en.wikipedia.org/wiki/Canada_Post
-    // https://en.wikipedia.org/wiki/Canadian_postal_abbreviations_for_provinces_and_territories#Names_and_abbreviations
-    CANADA(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Regions_of_Chile
-    CHILE(true, false, false, false, false, false, false),
-    CHINA(true, false, false, false, false, false, false),
-    COLOMBIA(true, false, false, false, false, false, false),
-    CROATIA(true, false, false, false, false, false, false),
-    CZECH_REPUBLIC(true, false, false, false, false, false, false),
-    DENMARK(true, false, false, false, false, false, false),
-    ESTONIA(true, false, false, false, false, false, false),
-    FINLAND(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postal_codes_in_France#CEDEX
-    FRANCE(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postal_codes_in_Germany
-    GERMANY(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postal_codes_in_Greece
-    GREECE(true, false, false, false, false, false, false),
-    HONG_KONG(true, false, false, false, false, false, false),
-    HUNGARY(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postal_codes_in_Iceland
-    ICELAND(true, false, false, false, false, false, false),
-    INDIA(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Subdivisions_of_Indonesia
-    INDONESIA(true, false, false, false, false, false, false),
-    IRAN(true, false, false, false, false, false, false),
-    IRAQ(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postal_addresses_in_the_Republic_of_Ireland
-    IRELAND(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postal_codes_in_Israel
-    ISRAEL(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Provinces_of_Italy#List_of_provinces
-    ITALY(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Japanese_addressing_system
-    JAPAN(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Dative_case
-    LATVIA(true, false, false, false, false, false, false),
-    MACAO(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Pos_Malaysia
-    // https://en.wikipedia.org/wiki/Postal_codes_in_Malaysia
-    MALAYSIA(true, false, false, false, false, false, false),
-    MEXICO(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postal_codes_in_the_Netherlands
-    // https://en.wikipedia.org/wiki/PostNL
-    NETHERLANDS(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postcodes_in_New_Zealand#Examples
-    NEW_ZEALAND(true, false, false, false, false, false, false),
-    NORWAY(true, false, false, false, false, false, false),
-    OMAN(true, false, false, false, false, false, false),
-    PAKISTAN(true, false, false, false, false, false, false),
-    PERU(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postal_addresses_in_the_Philippines
-    PHILIPPINES(true, false, false, false, false, false, false),
-    POLAND(true, false, false, false, false, false, false),
-    PORTUGAL(true, false, false, false, false, false, false),
-    QATAR(true, false, false, false, false, false, false),
-    ROMANIA(true, false, false, false, false, false, false),
-    RUSSIA(true, false, false, false, false, false, false),
-    SAUDI_ARABIA(true, false, false, false, false, false, false),
-    SERBIA(true, false, false, false, false, false, false),
-    SINGAPORE(true, false, false, false, false, false, false),
-    SLOVAKIA(true, false, false, false, false, false, false),
-    SLOVENIA(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Addresses_in_South_Korea
-    SOUTH_KOREA(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Provinces_of_Spain#Provinces
-    SPAIN(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Sri_Lanka_Post
-    SRI_LANKA(true, false, false, false, false, false, false),
-    SWEDEN(true, false, false, false, false, false, false),
-    SWITZERLAND(true, false, false, false, false, false, false),
-    TAIWAN(true, false, false, false, false, false, false),
-    THAILAND(true, false, false, false, false, false, false),
-    TURKEY(true, false, false, false, false, false, false),
-    UKRAINE(true, false, false, false, false, false, false),
-    UNITED_ARAB_EMIRATES(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom
-    // https://en.wikipedia.org/wiki/Postal_counties_of_the_United_Kingdom
-    UNITED_KINGDOM(true, false, false, false, false, false, false),
-    // https://en.wikipedia.org/wiki/List_of_U.S._state_and_territory_abbreviations
-    // https://en.wikipedia.org/wiki/ZIP_Code
-    // https://en.wikipedia.org/wiki/United_States_Postal_Service
-    UNITED_STATES(true, false, false, false, false, false, false),
-    VIETNAM(true, false, false, false, false, false, false)
-
-
-    AddressFormatSettings(
-            boolean houseNumberAfterStreetName,
-            boolean appendIso3166ToPostalCode,
-            boolean postalCodeBeforeLocality,
-            boolean lastLineCapitalLetters,
-            boolean unitSlashSeparated,
-            boolean unitsPriorToStreetNumber,
-            boolean twoSpaceAbbreviatedStateOrSuburb
-    ) {
-        this.houseNumberAfterStreetName = houseNumberAfterStreetName
-        this.appendIso3166ToPostalCode = appendIso3166ToPostalCode
-        this.postalCodeBeforeLocality = postalCodeBeforeLocality
-    }
-
-    boolean houseNumberAfterStreetName
-    boolean appendIso3166ToPostalCode
-    boolean postalCodeBeforeLocality
-    boolean lastLineCapitalLetters
-    boolean unitSlashSeparated
-    boolean unitsPriorToStreetNumber
-    boolean twoSpaceAbbreviatedStateOrSuburb
-
-    String convertTo(
-            Country origin,
-            Country destination,
-            String building,
-            String number,
-            String street,
-            String street2,
-            String street3,
-            String city,
-            String zip,
-            String state,
-            Country country,
-            String stateCode) {
-
-    }
-}
-
-@CompileStatic
 enum AddressFieldsSequence {
     // TODO: Complete cases from the listing up there
     DEFAULT_ONE(Fields.NUMBER, Fields.STREET, Fields.BUILDING, Fields.CITY, Fields.ZIP, Fields.STATE, Fields.COUNTRY)
@@ -211,22 +35,271 @@ enum AddressFieldsSequence {
     }
 }
 
+@CompileStatic
 interface StateBase {
+    Country getCountry()
     String getAbbrev()
-
     String getName()
 }
 
-// TODO: Need the same, first for all the biggest places (China, USA, India, Russia ...)
+/*
+    credit to Google Bard and ChatGPT for next lines
+*/
+
+@CompileStatic
 enum UsaStates implements StateBase {
-    TST("Test", "TST")
+    ALABAMA("AL", "Alabama"),
+    ALASKA("AK", "Alaska"),
+    ARIZONA("AZ", "Arizona"),
+    ARKANSAS("AR", "Arkansas"),
+    CALIFORNIA("CA", "California"),
+    COLORADO("CO", "Colorado"),
+    CONNECTICUT("CT", "Connecticut"),
+    DELAWARE("DE", "Delaware"),
+    FLORIDA("FL", "Florida"),
+    GEORGIA("GA", "Georgia"),
+    HAWAII("HI", "Hawaii"),
+    IDAHO("ID", "Idaho"),
+    ILLINOIS("IL", "Illinois"),
+    INDIANA("IN", "Indiana"),
+    IOWA("IA", "Iowa"),
+    KANSAS("KS", "Kansas"),
+    KENTUCKY("KY", "Kentucky"),
+    LOUISIANA("LA", "Louisiana"),
+    MAINE("ME", "Maine"),
+    MARYLAND("MD", "Maryland"),
+    MASSACHUSETTS("MA", "Massachusetts"),
+    MICHIGAN("MI", "Michigan"),
+    MINNESOTA("MN", "Minnesota"),
+    MISSISSIPPI("MS", "Mississippi"),
+    MISSOURI("MO", "Missouri"),
+    MONTANA("MT", "Montana"),
+    NEBRASKA("NE", "Nebraska"),
+    NEVADA("NV", "Nevada"),
+    NEW_HAMPSHIRE("NH", "New Hampshire"),
+    NEW_JERSEY("NJ", "New Jersey"),
+    NEW_MEXICO("NM", "New Mexico"),
+    NEW_YORK("NY", "New York"),
+    NORTH_CAROLINA("NC", "North Carolina"),
+    NORTH_DAKOTA("ND", "North Dakota"),
+    OHIO("OH", "Ohio"),
+    OKLAHOMA("OK", "Oklahoma"),
+    OREGON("OR", "Oregon"),
+    PENNSYLVANIA("PA", "Pennsylvania"),
+    RHODE_ISLAND("RI", "Rhode Island"),
+    SOUTH_CAROLINA("SC", "South Carolina"),
+    SOUTH_DAKOTA("SD", "South Dakota"),
+    TENNESSEE("TN", "Tennessee"),
+    TEXAS("TX", "Texas"),
+    UTAH("UT", "Utah"),
+    VERMONT("VT", "Vermont"),
+    VIRGINIA("VA", "Virginia"),
+    WASHINGTON("WA", "Washington"),
+    WEST_VIRGINIA("WV", "West Virginia"),
+    WISCONSIN("WI", "Wisconsin"),
+    WYOMING("WY", "Wyoming")
 
     UsaStates(String name, String abbrev) {
         this.name = name
         this.abbrev = abbrev
     }
 
-    String name
-    String abbrev
+    Country getCountry() {
+        Country.US
+    }
+
+    final String abbrev
+    final String name
+}
+
+@CompileStatic
+enum IndianStates implements StateBase {
+    AP("Andhra Pradesh", "AP"),
+    AR("Arunachal Pradesh", "AR"),
+    AS("Assam", "AS"),
+    BR("Bihar", "BR"),
+    CG("Chhattisgarh", "CG"),
+    GA("Goa", "GA"),
+    GJ("Gujarat", "GJ"),
+    HR("Haryana", "HR"),
+    HP("Himachal Pradesh", "HP"),
+    JH("Jharkhand", "JH"),
+    KA("Karnataka", "KA"),
+    KL("Kerala", "KL"),
+    MP("Madhya Pradesh", "MP"),
+    MH("Maharashtra", "MH"),
+    MN("Manipur", "MN"),
+    ML("Meghalaya", "ML"),
+    MZ("Mizoram", "MZ"),
+    NL("Nagaland", "NL"),
+    OD("Odisha", "OD"),
+    PB("Punjab", "PB"),
+    RJ("Rajasthan", "RJ"),
+    SK("Sikkim", "SK"),
+    TN("Tamil Nadu", "TN"),
+    TS("Telangana", "TS"),
+    TR("Tripura", "TR"),
+    UP("Uttar Pradesh", "UP"),
+    UK("Uttarakhand", "UK"),
+    WB("West Bengal", "WB")
+
+    IndianStates(String name, String abbrev) {
+        this.name = name
+        this.abbrev = abbrev
+    }
+
+    Country getCountry() {
+        Country.IN
+    }
+
+    final String abbrev
+    final String name
+}
+
+@CompileStatic
+enum CanadaStates implements StateBase {
+    ALBERTA("AB", "Alberta"),
+    BRITISH_COLUMBIA("BC", "British Columbia"),
+    MANITOBA("MB", "Manitoba"),
+    NEW_BRUNSWICK("NB", "New Brunswick"),
+    NEWFOUNDLAND_AND_LABRADOR("NL", "Newfoundland and Labrador"),
+    NORTHWEST_TERRITORIES("NT", "Northwest Territories"),
+    NOVA_SCOTIA("NS", "Nova Scotia"),
+    NUNAVUT("NU", "Nunavut"),
+    ONTARIO("ON", "Ontario"),
+    PRINCE_EDWARD_ISLAND("PE", "Prince Edward Island"),
+    QUEBEC("QC", "Quebec"),
+    SASKATCHEWAN("SK", "Saskatchewan"),
+    YUKON("YT", "Yukon")
+
+    CanadaStates(String name, String abbrev) {
+        this.name = name
+        this.abbrev = abbrev
+    }
+
+    Country getCountry() {
+        Country.CA
+    }
+
+    final String abbrev
+    final String name
+}
+
+@CompileStatic
+enum ChinaStates implements StateBase {
+    ANHUI("AH", "Anhui"),
+    BEIJING("BJ", "Beijing"),
+    CHONGQING("CQ", "Chongqing"),
+    FUJIAN("FJ", "Fujian"),
+    GANSU("GS", "Gansu"),
+    GUANGDONG("GD", "Guangdong"),
+    GUANGXI("GX", "Guangxi"),
+    GUIZHOU("GZ", "Guizhou"),
+    HAINAN("HI", "Hainan"),
+    HEBEI("HE", "Hebei"),
+    HEILONGJIANG("HL", "Heilongjiang"),
+    HENAN("HA", "Henan"),
+    HUBEI("HB", "Hubei"),
+    HUNAN("HN", "Hunan"),
+    JIANGSU("JS", "Jiangsu"),
+    JIANGXI("JX", "Jiangxi"),
+    JILIN("JL", "Jilin"),
+    LIAONING("LN", "Liaoning"),
+    NEI_MONGOL("NM", "Nei Mongol"),
+    NINGXIA("NX", "Ningxia"),
+    QINGHAI("QH", "Qinghai"),
+    SHAANXI("SN", "Shaanxi"),
+    SHANDONG("SD", "Shandong"),
+    SHANGHAI("SH", "Shanghai"),
+    SHANXI("SX", "Shanxi"),
+    SICHUAN("SC", "Sichuan"),
+    TIANJIN("TJ", "Tianjin"),
+    XINJIANG("XJ", "Xinjiang"),
+    XIZANG("XZ", "Xizang"),
+    YUNNAN("YN", "Yunnan"),
+    ZHEJIANG("ZJ", "Zhejiang")
+
+    ChinaStates(String name, String abbrev) {
+        this.name = name
+        this.abbrev = abbrev
+    }
+
+    Country getCountry() {
+        Country.CN
+    }
+
+    final String abbrev
+    final String name
+}
+
+@CompileStatic
+enum MexicoStates implements StateBase {
+    AGUASCALIENTES("AGU", "Aguascalientes"),
+    BAJA_CALIFORNIA("BCN", "Baja California"),
+    BAJA_CALIFORNIA_SUR("BCS", "Baja California Sur"),
+    CAMPECHE("CAM", "Campeche"),
+    CHIAPAS("CHP", "Chiapas"),
+    CHIHUAHUA("CHH", "Chihuahua"),
+    COAHUILA("COA", "Coahuila"),
+    COLIMA("COL", "Colima"),
+    DURANGO("DUR", "Durango"),
+    GUANAJUATO("GUA", "Guanajuato"),
+    GUERRERO("GRO", "Guerrero"),
+    HIDALGO("HID", "Hidalgo"),
+    JALISCO("JAL", "Jalisco"),
+    MEXICO("MEX", "México"),
+    MICHOACAN("MIC", "Michoacán"),
+    MORELOS("MOR", "Morelos"),
+    NAYARIT("NAY", "Nayarit"),
+    NUEVO_LEON("NLE", "Nuevo León"),
+    OAXACA("OAX", "Oaxaca"),
+    PUEBLA("PUE", "Puebla"),
+    QUERETARO("QUE", "Querétaro"),
+    QUINTANA_ROO("ROO", "Quintana Roo"),
+    SAN_LUIS_POTOSI("SLP", "San Luis Potosí"),
+    SINALOA("SIN", "Sinaloa"),
+    SONORA("SON", "Sonora"),
+    TABASCO("TAB", "Tabasco"),
+    TAMAULIPAS("TAM", "Tamaulipas"),
+    TLAXCALA("TLA", "Tlaxcala"),
+    VERACRUZ("VER", "Veracruz"),
+    YUCATAN("YUC", "Yucatán"),
+    ZACATECAS("ZAC", "Zacatecas")
+
+    MexicoStates(String name, String abbrev) {
+        this.name = name
+        this.abbrev = abbrev
+    }
+
+    Country getCountry() {
+        Country.CN
+    }
+
+    final String abbrev
+    final String name
+}
+
+@CompileStatic
+final class ManagedStates {
+
+    static StateBase[] getStates(Country country) {
+        switch (country) {
+            case Country.US:
+                return UsaStates.values()
+            case Country.CA:
+                return CanadaStates.values()
+            case Country.CN:
+                return ChinaStates.values()
+            case Country.ME:
+                return MexicoStates.values()
+        }
+        return null
+    }
+
+    static StateBase getState(Country country, String stateAbbrev) {
+        getStates(country).find {
+            it.abbrev == stateAbbrev
+        }
+    }
 
 }
