6a818463fb4c5a3072db62d30cf929020199f725
Adrien Guichard <adrien.guichard@citel.fr>
2024-05-05 04:34:00 +0200

    add labels to SolrFieldType

--- taack-ui/src/main/groovy/taack/solr/SolrFieldType.groovy
+++ taack-ui/src/main/groovy/taack/solr/SolrFieldType.groovy
@@ -7,32 +7,34 @@ import groovy.transform.CompileStatic
  */
 @CompileStatic
 enum SolrFieldType {
-    POINT_INT("_i"),    // for ints
-    POINT_STRING("_s"), // for strings
-    POINT_LONG("_l"),   // ... long
-    TXT_UNIQ("_t"),
-    TXT_GENERAL("_t"),  // lower case
-    TXT_GENERAL_MULTI("_txt"),  // lower case
-    TXT_NO_ACCENT("_noAccent"),  // lower case
-    TXT_GENERAL_EN("_txt_en"),  // lower case
-    TXT_GENERAL_DE("_txt_de"),  // lower case
-    TXT_GENERAL_ES("_txt_es"),  // lower case
-    TXT_GENERAL_FR("_txt_fr"),  // lower case
-    TXT_GENERAL_IT("_txt_it"),  // lower case
-    TXT_GENERAL_RU("_txt_ru"),  // lower case
-    TXT_GENERAL_EN_SPLIT("_txt_en_split"),  // lower case
-    TXT_PHONE_EN("_phon_en"),  // lower case
-    TXT_WS("_ws"),      // white space
-    TXT_SORT("_t_sort"),// AFAIU for faceting
-    BOOLEAN("_b"),
-    POINT_FLOAT("_f"),
-    DATE("_dt"),
-    LOCATION("_p"),
-    POINT_DOUBLE("_d")
+    POINT_INT('_i', 'NearestInts'),    // for ints
+    POINT_STRING('_s', 'ExactStr'), // for strings
+    POINT_LONG('_l', 'NearestLongs'),   // ... long
+    TXT_UNIQ('_t', 'ExactTxt'),
+    TXT_GENERAL('_t', 'ExactTxt'),  // lower case
+    TXT_GENERAL_MULTI('_txt', 'SimplifiedTxt'),  // lower case
+    TXT_NO_ACCENT('_noAccent', 'TxtWoutAccent'),  // lower case
+    TXT_GENERAL_EN('_txt_en', 'EnTxt'),  // lower case
+    TXT_GENERAL_DE('_txt_de', 'DeTxt'),  // lower case
+    TXT_GENERAL_ES('_txt_es', 'EsTxt'),  // lower case
+    TXT_GENERAL_FR('_txt_fr', 'FrTxt'),  // lower case
+    TXT_GENERAL_IT('_txt_it', 'ItTxt'),  // lower case
+    TXT_GENERAL_RU('_txt_ru', 'RuTxt'),  // lower case
+    TXT_GENERAL_EN_SPLIT('_txt_en_split', 'SplitEnTxt'),  // lower case
+    TXT_PHONE_EN('_phon_en', 'PhoneticEnTxt'),  // lower case
+    TXT_WS('_ws', 'NoWSTxt'),      // white space
+    TXT_SORT('_t_sort', 'Txt4Facets'),// AFAIU for faceting
+    BOOLEAN('_b', 'Bool'),
+    POINT_FLOAT('_f', 'Float'),
+    DATE('_dt', 'Date'),
+    LOCATION('_p', 'Location'),
+    POINT_DOUBLE('_d', 'PreciseFloat')
 
-    SolrFieldType(String suffix) {
+    SolrFieldType(String suffix, String label) {
         this.suffix = suffix
+        this.label = label
     }
 
     final String suffix
+    final String label
 }
