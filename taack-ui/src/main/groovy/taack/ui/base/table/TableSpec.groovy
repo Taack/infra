4e2a25798e7a201d4088d87c4a5b3345f8e2bdcd
Adrien Guichard <adrien.guichard@taack.org>
2024-05-11 05:33:51 +0200

    fix iterating

--- taack-ui/src/main/groovy/taack/ui/base/table/TableSpec.groovy
+++ taack-ui/src/main/groovy/taack/ui/base/table/TableSpec.groovy
@@ -95,12 +95,15 @@ final class TableSpec {
         tableVisitor.visitRowEnd()
     }
 
-    final<T extends GormEntity> Long iterate(TaackFilter<T> taackFilter, @DelegatesTo(strategy = Closure.DELEGATE_FIRST, value = RowColumnSpec.class) Closure c) {
-        c.delegate = new TableSpec(tableVisitor)
+    final<T extends GormEntity> Long iterate(TaackFilter<T> taackFilter, @DelegatesTo(strategy = Closure.DELEGATE_FIRST, value = RowColumnSpec) Closure c) {
+        c.delegate = new RowColumnSpec(tableVisitor)
         Pair<List<T>, Long> res = taackFilter.list()
         tableVisitor.visitPaginate(taackFilter.max, res.bValue)
-        for (T t in res.aValue)
+        for (T t in res.aValue) {
+            tableVisitor.visitRow(null, false)
             c.call(t)
+            tableVisitor.visitRowEnd()
+        }
         res.bValue
     }
 
