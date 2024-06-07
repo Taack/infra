1a83a950305eb3bb9c53374bd3870a87583d18d7
Adrien Guichard <adrien.guichard@citel.fr>
2024-06-07 17:55:17 +0200

    clean up

--- taack-ui-test/grails-app/controllers/taack/ui/test/ActionDefaultLogInterceptor.groovy
+++ taack-ui-test/grails-app/controllers/taack/ui/test/ActionDefaultLogInterceptor.groovy
@@ -2,8 +2,6 @@ package taack.ui.test
 
 import grails.artefact.Interceptor
 import grails.compiler.GrailsCompileStatic
-import grails.plugin.springsecurity.SpringSecurityService
-import org.grails.web.util.WebUtils
 
 @GrailsCompileStatic
 class ActionDefaultLogInterceptor implements Interceptor {
@@ -11,8 +9,6 @@ class ActionDefaultLogInterceptor implements Interceptor {
     ActionDefaultLogInterceptor() {
         matchAll().excludes(controller:"login")
     }
-
-    SpringSecurityService springSecurityService
 
     boolean before() {
         println "$params"
