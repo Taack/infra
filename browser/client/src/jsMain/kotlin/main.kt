17638ff550ade86111e3c09faef5d3e5e98d993f
Adrien Guichard <adrien.guichard@citel.fr>
2024-07-02 03:19:56 +0200

    avoid login multiple times

--- browser/client/src/jsMain/kotlin/main.kt
+++ browser/client/src/jsMain/kotlin/main.kt
@@ -2,7 +2,9 @@ import kotlinx.browser.window
 import taack.ui.base.element.Block
 
 fun main() {
-    Block.href = window.location.href
-    Block.getSiblingBlock(null)
-    window.addEventListener("popstate", { window.location.reload() })
+    if (!window.location.href.contains("login")) {
+        Block.href = window.location.href
+        Block.getSiblingBlock(null)
+        window.addEventListener("popstate", { window.location.reload() })
+    }
 }
