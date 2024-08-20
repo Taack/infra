0cc57bc9cb9daf6b0d4b0dc03d6c2b12dec6cdb0
Adrien Guichard <adrien.guichard@citel.fr>
2024-08-20 05:24:31 +0200

    clean up code before 0.5.3 release

--- taack-ui/src/main/groovy/taack/domain/IDomainToGantt.groovy
+++
@@ -1,7 +0,0 @@
-package taack.domain
-
-import taack.domain.graph.gantt.GanttEntry
-
-interface IDomainToGantt {
-    List<GanttEntry> extractGantt()
-}
--- taack-ui/src/main/groovy/taack/domain/graph/timing/TimingSeriesEntry.groovy
+++
@@ -1,9 +0,0 @@
-package taack.domain.graph.timing
-
-import groovy.transform.CompileStatic
-
-@CompileStatic
-class TimingSeriesEntry {
-    Date date
-    String status
-}
