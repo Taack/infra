3fa3db1b06359ea6efc85f8d1c295dc7eaa43fdc
Adrien Guichard <adrien.guichard@citel.fr>
2024-09-10 01:22:53 +0200

    improve modals

--- browser/client/src/jsMain/kotlin/taack/ui/base/element/Modal.kt
+++ browser/client/src/jsMain/kotlin/taack/ui/base/element/Modal.kt
@@ -3,6 +3,7 @@ package taack.ui.base.element
 import kotlinx.browser.document
 import kotlinx.dom.addClass
 import org.w3c.dom.HTMLAnchorElement
+import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.HTMLDivElement
 import taack.ui.base.BaseElement
 import taack.ui.base.Helper.Companion.trace
@@ -34,39 +35,49 @@ class Modal(val parent: Block) : BaseElement {
         dModalContent = document.createElement("div") as HTMLDivElement
         dModalBody = document.createElement("div") as HTMLDivElement
         dModalBody.addClass("modal-body", "overflow-y-auto")
-//        val innerModal = document.createElement("div") as HTMLDivElement
         dModalContent.classList.add("modal-content")
         dModalContent.classList.add("taackModal")
-        val a = document.createElement("a") as HTMLAnchorElement
         val dClose = document.createElement("div") as HTMLDivElement
-        a.innerText = "X"
-        a.addClass("close")
-        a.onclick = { _ ->
+        val closeButton = document.createElement("button") as HTMLButtonElement
+        closeButton.type = "button"
+        closeButton.className = "btn-close"
+        closeButton.onclick = { e ->
+            e.preventDefault()
             close()
         }
+
         dClose.addClass("taack-close")
         dClose.addClass("modal-header")
-        dClose.appendChild(a)
+        dClose.appendChild(closeButton)
         dModalContent.appendChild(dClose)
         dModalContent.appendChild(dModalBody)
-//        dModalContent.appendChild(innerModal)
         dModalDialog.appendChild(dModalContent)
         dModal.appendChild(dModalDialog)
         parent.d.parentElement?.appendChild(dModal)
     }
 
     fun open(htmlContent: String) {
-        traceIndent("Modal::open $id")
+        traceIndent("Modal::open $mId")
         dModalBody.innerHTML = htmlContent
         dModal.style.display = "block"
         Block.getSiblingBlock(this)
+        document.body!!.classList.add("modal-open")
+        document.body!!.style.paddingRight = "15px"
+        document.body!!.style.overflowY = "hidden"
+        val modalBackdrop = document.createElement("div") as HTMLDivElement
+        modalBackdrop.id = "modal-backdrop-$mId"
+        modalBackdrop.addClass("modal-backdrop", "fade", "show")
+        document.body!!.appendChild(modalBackdrop)
     }
 
     fun close() {
         traceDeIndent("Modal::close $mId")
         dModal.style.display = "none"
         dModalBody.innerHTML = ""
-//        if (parent.parent != null) d1.remove()
+        document.body!!.classList.remove("modal-open")
+        document.body!!.style.removeProperty("padding-right")
+        document.body!!.style.removeProperty("overflow-y")
+        document.getElementById("modal-backdrop-$mId")?.remove()
     }
 
     override fun getParentBlock(): Block {
