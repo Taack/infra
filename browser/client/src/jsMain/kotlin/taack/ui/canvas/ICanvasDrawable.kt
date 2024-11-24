f4d12932463ffec7d9a7fe3914610da8241ef3f3
Adrien Guichard <adrien.guichard@citel.fr>
2024-11-24 19:17:11 +0100

    WiP Asciidoc Parser

--- browser/client/src/jsMain/kotlin/taack/ui/canvas/ICanvasDrawable.kt
+++ browser/client/src/jsMain/kotlin/taack/ui/canvas/ICanvasDrawable.kt
@@ -1,9 +1,28 @@
 package taack.ui.canvas
 
 import taack.ui.canvas.text.CanvasText
+import taack.ui.canvas.text.H2Canvas
+import taack.ui.canvas.text.H3Canvas
+import taack.ui.canvas.text.H4Canvas
 import web.canvas.CanvasRenderingContext2D
 
 interface ICanvasDrawable: ICanvasSelectable {
+
+    enum class AsciidocToken(exp: Regex) {
+        H2(Regex("^== ")),
+        H3(Regex("^=== ")),
+        H4(Regex("^==== ")),
+        B1(Regex("^\\*")),
+        B2(Regex("^\\*\\* ")),
+        TABLE_START(Regex("^\\|===")),
+        TABLE_COL(Regex("\\|([^|]*)")),
+        TABLE_CELL(Regex("^\\|([^|]*)")),
+        BOLD(Regex("\\*\\*")),
+        MONO(Regex("`")),
+        NEXT_DRAWABLE(Regex("\n\n")),
+        NEXT_LINE(Regex("\n")),
+        P(Regex(".*")),
+    }
 
     companion object {
         fun dumpAsciidoc(drawables: List<ICanvasDrawable>): String {
@@ -25,7 +44,20 @@ interface ICanvasDrawable: ICanvasSelectable {
             return out.toString()
         }
 
-
+        fun readAsciidoc(file: String): List<ICanvasDrawable> {
+            val drawables = file.substring(file.indexOf("\n\n")).split("\n")
+            val canvasDrawables = mutableListOf<ICanvasDrawable>()
+            for (drawable in drawables) {
+                if (drawable.startsWith("== ")) {
+                    canvasDrawables.add(H2Canvas(drawable.substring(3)))
+                } else if (drawable.startsWith("=== ")) {
+                    canvasDrawables.add(H3Canvas(drawable.substring(4)))
+                } else if (drawable.startsWith("==== ")) {
+                    canvasDrawables.add(H4Canvas(drawable.substring(5)))
+                }
+            }
+            return canvasDrawables
+        }
 
     }
 
