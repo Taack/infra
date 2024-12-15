880ea4d70c192ec3953a06cb9d08c94f99d0bd78
Adrien Guichard <adrien.guichard@citel.fr>
2024-12-15 03:40:09 +0100

    clean up code

--- taack-ui/src/main/groovy/taack/ast/type/FieldInfo.groovy
+++ taack-ui/src/main/groovy/taack/ast/type/FieldInfo.groovy
@@ -53,92 +53,92 @@ final class FieldInfo<T> {
         "FieldInfo{fieldConstraint=$fieldConstraint, fieldName=$fieldName value=$value}"
     }
 
-    /**
-     * Create a FieldInfo from another FieldInfo that point to a map, using its key.
-     *
-     * @param fieldInfo
-     * @param key
-     * @return FieldInfo for the key
-     */
-    final static FieldInfo mapKey(final FieldInfo fieldInfo, final String key) {
-        new FieldInfo(fieldInfo.fieldConstraint, fieldInfo.fieldName + "_setKey_$key", fieldInfo.value)
-    }
+//    /**
+//     * Create a FieldInfo from another FieldInfo that point to a map, using its key.
+//     *
+//     * @param fieldInfo
+//     * @param key
+//     * @return FieldInfo for the key
+//     */
+//    final static FieldInfo mapKey(final FieldInfo fieldInfo, final String key) {
+//        new FieldInfo(fieldInfo.fieldConstraint, fieldInfo.fieldName + "_setKey_$key", fieldInfo.value)
+//    }
+//
+//    /**
+//     * Create a FieldInfo from another FieldInfo that point to a map, using its key.
+//     *
+//     * @param fieldInfo
+//     * @param key
+//     * @return FieldInfo for the value pointed by the map entry
+//     */
+//    final static FieldInfo mapValue(final FieldInfo fieldInfo, final String key) {
+//        // TODO: Should be new FieldInfo(fieldInfo.fieldConstraint, fieldInfo.fieldName + "_setValue_$key", fieldInfo.value[key])
+//        new FieldInfo(fieldInfo.fieldConstraint, fieldInfo.fieldName + "_setValue_$key", fieldInfo.value)
+//    }
 
-    /**
-     * Create a FieldInfo from another FieldInfo that point to a map, using its key.
-     *
-     * @param fieldInfo
-     * @param key
-     * @return FieldInfo for the value pointed by the map entry
-     */
-    final static FieldInfo mapValue(final FieldInfo fieldInfo, final String key) {
-        // TODO: Should be new FieldInfo(fieldInfo.fieldConstraint, fieldInfo.fieldName + "_setValue_$key", fieldInfo.value[key])
-        new FieldInfo(fieldInfo.fieldConstraint, fieldInfo.fieldName + "_setValue_$key", fieldInfo.value)
-    }
+//    final boolean isMapKey() {
+//        fieldName.contains("_setKey_")
+//    }
 
-    final boolean isMapKey() {
-        fieldName.contains("_setKey_")
-    }
+//    final boolean isMapValue() {
+//        fieldName.contains("_setValue_")
+//    }
 
-    final boolean isMapValue() {
-        fieldName.contains("_setValue_")
-    }
+//    /**
+//     * @return Name of the field that contains the map.
+//     */
+//    final String getMapFieldName() {
+//        if (isMapKey()) {
+//            int iPoint = fieldName.indexOf('_setKey_')
+//            fieldName.substring(0, iPoint)
+//        } else if (isMapValue()) {
+//            int iPoint = fieldName.indexOf('_setValue_')
+//            fieldName.substring(0, iPoint)
+//        } else fieldName
+//    }
+//
+//    /**
+//     * @return The key value.
+//     */
+//    final String getMapFieldKey() {
+//        if (isMapKey()) {
+//            int iPoint = fieldName.indexOf('_setKey_')
+//            fieldName.substring(iPoint + 8)
+//
+//        } else if (isMapValue()) {
+//            int iPoint = fieldName.indexOf('_setValue_')
+//            fieldName.substring(iPoint + 10)
+//        } else null
+//    }
 
-    /**
-     * @return Name of the field that contains the map.
-     */
-    final String getMapFieldName() {
-        if (isMapKey()) {
-            int iPoint = fieldName.indexOf('_setKey_')
-            fieldName.substring(0, iPoint)
-        } else if (isMapValue()) {
-            int iPoint = fieldName.indexOf('_setValue_')
-            fieldName.substring(0, iPoint)
-        } else fieldName
-    }
+//    /**
+//     * @return The object value.
+//     */
+//    final Object getMapValue() {
+//        if (isMapValue()) {
+//            int iPoint = fieldName.indexOf('_setValue_')
+//            return value[fieldName.substring(iPoint + 10)]
+//        }
+//        null
+//    }
 
-    /**
-     * @return The key value.
-     */
-    final String getMapFieldKey() {
-        if (isMapKey()) {
-            int iPoint = fieldName.indexOf('_setKey_')
-            fieldName.substring(iPoint + 8)
-
-        } else if (isMapValue()) {
-            int iPoint = fieldName.indexOf('_setValue_')
-            fieldName.substring(iPoint + 10)
-        } else null
-    }
-
-    /**
-     * @return The object value.
-     */
-    final Object getMapValue() {
-        if (isMapValue()) {
-            int iPoint = fieldName.indexOf('_setValue_')
-            return value[fieldName.substring(iPoint + 10)]
-        }
-        null
-    }
-
-    /**
-     * If value point to an object, or a map, by name
-     * @param name
-     * @return the value of object.name
-     */
-    final String getEmbeddedValue(String name) {
-        String res
-        if (name) {
-            res = value[name]
-        } else if (isMapValue()) {
-            res = getMapValue()
-        } else if (isMapKey()) {
-            res = getMapFieldKey()
-        } else {
-            res = value
-        }
-        res = res ?: ''
-        res
-    }
+//    /**
+//     * If value point to an object, or a map, by name
+//     * @param name
+//     * @return the value of object.name
+//     */
+//    final String getEmbeddedValue(String name) {
+//        String res
+//        if (name) {
+//            res = value[name]
+//        } else if (isMapValue()) {
+//            res = getMapValue()
+//        } else if (isMapKey()) {
+//            res = getMapFieldKey()
+//        } else {
+//            res = value
+//        }
+//        res = res ?: ''
+//        res
+//    }
 }
