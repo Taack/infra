5295875566a5307c9410fcb374626e97291e07d4
Luigi Ribbi <luigi.ribbi@citel.fr>
2025-01-31 09:12:18 +0100

    Lazy load tab enhancements, load spinner, caching tabs

--- browser/client/src/jsMain/kotlin/taack/ui/base/leaf/TabButton.kt
+++ browser/client/src/jsMain/kotlin/taack/ui/base/leaf/TabButton.kt
@@ -2,18 +2,15 @@ package taack.ui.base.leaf
 
 import js.array.asList
 import taack.ui.base.BaseElement
-import taack.ui.base.Helper
-import taack.ui.base.Helper.Companion.mapAjaxBlock
-import taack.ui.base.Helper.Companion.processAjaxLink
 import taack.ui.base.Helper.Companion.traceIndent
 import taack.ui.base.element.AjaxBlock
 import taack.ui.base.element.Block
 import taack.ui.base.element.Tab
 import web.dom.Element
-import web.dom.document
+import web.dom.parsing.DOMParser
+import web.dom.parsing.DOMParserSupportedType
 import web.events.EventHandler
 import web.form.FormData
-import web.form.FormMethod
 import web.html.*
 import web.http.RequestMethod
 import web.xhr.XMLHttpRequest
@@ -32,20 +29,34 @@ class TabButton(val parent: Tab, val b: HTMLButtonElement) : BaseElement  {
         traceIndent("TabButton::init +++ id: ${b.id}")
         b.onclick = EventHandler { e ->
             e.preventDefault()
+            val tabIndex = b.getAttribute("id")!!.split("-").last()
+            val tabId = b.getAttribute("id")!!.split("-")[1]
             val fd = FormData()
             fd["isAjax"] = "true"
             fd["refresh"] = "true"
-            fd["tabIndex"] = b.getAttribute("id")!!.split("-").last()
-            fd["tabId"] = b.getAttribute("id")!!.split("-")[1]
+            fd["tabIndex"] = tabIndex
+            fd["tabId"] = tabId
             val xhr = XMLHttpRequest()
+            xhr.onloadstart = EventHandler {
+                b.onclick = EventHandler { console.log("Tab already requested")}
+            }
             xhr.onloadend = EventHandler {
+                // Make new parser to parse xhr.responseText
+                val parser = DOMParser()
+                // Parse to a DOM document to allow query selection
+                val responseDoc = parser.parseFromString(xhr.responseText, DOMParserSupportedType.textHtml)
+                // Get the clicked tab from the response
+                val tabResponseEl = responseDoc.querySelector("#tab-${tabId}-${tabIndex}-pane")
                 val div: Element? = parent.d.querySelector(".tab-content")
                 println("div: ${div?.id}")
-                if (div != null) {
-                    div.innerHTML = xhr.responseText
+                if (div != null && tabResponseEl != null) {
+                    // Get the clicked tab content pane and fill it with the previously parsed response's inner html
+                    div.querySelector("#tab-${tabId}-${tabIndex}-pane")?.innerHTML = tabResponseEl.innerHTML
                     AjaxBlock.getSiblingAjaxBlock(parent.parent)
                 }
             }
+            //Show loading spinner while loading
+            parent.d.querySelector("#tab-${tabId}-${tabIndex}-pane")?.innerHTML = "<div class='taack-tab-load'></div>"
             xhr.open(RequestMethod.POST, b.getAttribute("action")!!)
             xhr.send(fd)
         }
