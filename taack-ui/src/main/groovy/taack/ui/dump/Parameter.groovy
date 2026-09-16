2e684a9b86d9e6fd8286dde802d36d7dbda0c27a
ZhenQing Yu <zhenqing.yu@citel.fr>
2026-09-16 10:03:58 +0800

    fix missing type of Number in validateableToParamsToKeep

--- taack-ui/src/main/groovy/taack/ui/dump/Parameter.groovy
+++ taack-ui/src/main/groovy/taack/ui/dump/Parameter.groovy
@@ -233,7 +233,7 @@ final class Parameter implements WebAttributes {
     final static <T extends Validateable> List<String> validateableToParamsToKeep(T validateable) {
         List<String> ret = []
         validateable.class.getDeclaredFields().each {
-            if (!it.name.contains('_') && !it.name.contains('$') && ([Boolean, String, Date, Integer, Long, Short].contains(it.type) || it.type.isEnum() || GormEntity.isAssignableFrom(it.type) || Collection.isAssignableFrom(it.type))) {
+            if (!it.name.contains('_') && !it.name.contains('$') && ([Boolean, String, Date].contains(it.type) || Number.isAssignableFrom(it.type) || it.type.isEnum() || GormEntity.isAssignableFrom(it.type) || Collection.isAssignableFrom(it.type))) {
                 ret.add(it.name)
             }
         }
